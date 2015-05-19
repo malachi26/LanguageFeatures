7dd39d67f8e163b3380e57193b6fa776fb079e0b
malachi26 <lyle.hart.3@gmail.com>
2015-05-19 10:59:19 -0500

    Page 72

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -17,5 +17,19 @@ namespace LanguageFeatures.Controllers
             return "Navigate to a URL to show an Example";
         }
 
+        public ViewResult AutoProperty()
+        {
+            // create a new Product object
+            Product myProduct = new Product();
+
+            //set the property value
+            myProduct.Name = "Kayak";
+
+            //get the property
+            string productName = myProduct.Name;
+
+            // Generate the view
+            return View("result", (object)String.Format("Product name: {0}", productName));
+        }
     }
 }
